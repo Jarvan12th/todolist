17ccf43cba5798f013d814b9ddbe8bf630fc8939
jarvan.li <jarvan.li@derbysoft.net>
2022-12-01 22:57:48 +0800

    introduce ref

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,9 @@
         <nav>header</nav>
         <nav>main</nav>
         <nav>footer</nav>
+        <div>A week contains {{ num }} days</div>
+        <div>They are {{ arr }}</div>
+        <div>Today is {{ today }}, {{ obj.date }}</div>
     </div>
 </template>
 
@@ -10,7 +13,7 @@
 import NavHeaderVue from '@/components/navHeader/NavHeader.vue';
 import NavMainVue from '@/components/navMain/NavMain.vue';
 import NavFooterVue from '@/components/navFooter/NavFooter.vue';
-import {defineComponent} from 'vue'
+import {defineComponent, ref} from 'vue'
 export default defineComponent({
     name: 'Home',
     props: {},
@@ -20,7 +23,18 @@ export default defineComponent({
         NavFooterVue
     },
     setup(props, ctx) {
-        return {}
+        let num = ref(7)
+        let arr = ref(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+        let today = ref('Thursday')
+        let obj = ref({
+            date: '2022-12-1'
+        })
+        return {
+            num,
+            arr,
+            today,
+            obj
+        }
     }
 })
 </script>
