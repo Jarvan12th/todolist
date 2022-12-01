571d35111c2525f308f29f2a178c8c0c718d4bce
jarvan.li <jarvan.li@derbysoft.net>
2022-12-01 21:57:28 +0800

    refactor HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+import {defineComponent} from 'vue'
+export default defineComponent({
+    name: 'Home',
+    props: {},
+    components: {},
+    setup(props, ctx) {
+        return {}
+    }
+})
 </script>
+
+<style scoped lang="SCSS">
+
+</style>
