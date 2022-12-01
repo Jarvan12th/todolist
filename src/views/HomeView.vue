264d0c5924c80bb9d3c964dd37dca064d5a2f7a6
jarvan.li <jarvan.li@derbysoft.net>
2022-12-01 22:39:07 +0800

    add three child components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,24 @@
 <template>
-
+    <div>
+        <nav>header</nav>
+        <nav>main</nav>
+        <nav>footer</nav>
+    </div>
 </template>
 
 <script>
+import NavHeaderVue from '@/components/navHeader/NavHeader.vue';
+import NavMainVue from '@/components/navMain/NavMain.vue';
+import NavFooterVue from '@/components/navFooter/NavFooter.vue';
 import {defineComponent} from 'vue'
 export default defineComponent({
     name: 'Home',
     props: {},
-    components: {},
+    components: {
+        NavHeaderVue,
+        NavMainVue,
+        NavFooterVue
+    },
     setup(props, ctx) {
         return {}
     }
