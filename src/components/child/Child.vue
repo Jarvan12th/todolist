854e1b346dcd0ab4e3c1904e1399013a25f81c79
jarvan.li <jarvan.li@derbysoft.net>
2022-12-05 02:02:38 +0800

    feature(views): passing data between components

---
+++ src/components/child/Child.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>This is a child page</h1>
+        <div>Data from detail page: {{ msg }}</div>
+        <button @click="send">send</button>
+    </div>
+</template>
+
+<script>
+import { defineComponent, onMounted, ref } from 'vue';
+
+export default defineComponent({
+    name: 'child',
+    props: {
+        msg: {
+            typeof: String
+        }
+    },
+    setup(props, ctx) {
+        let childMsg = ref('message sent from child page')
+        let send = () => {
+            ctx.emit('send', childMsg.value)
+        }
+        onMounted(() => {
+            ctx.emit('send', childMsg.value)
+        })
+        return {
+            childMsg,
+            send
+        }
+    }
+})
+</script>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,5 +1,29 @@
 <template>
-    <div class="detail">
+    <div>
         <h1>This is a detail page</h1>
+        <child :msg="msg" @send="send"></child>
     </div>
 </template>
+
+<script>
+import { send } from 'process';
+import { defineComponent, ref } from 'vue';
+import child from '../components/child/Child.vue'
+
+export default defineComponent({
+    name: 'detail',
+    components: {
+        child
+    },
+    setup() {
+        let msg = ref('message in detail')
+        let send = (val) => {
+            console.log(val)
+        }
+        return {
+            msg,
+            send
+        }
+    }
+})
+</script>
