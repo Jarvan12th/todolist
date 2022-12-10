7ec26741cc5735851fb2e2b81c1f702b618ce22b
jarvan.li <jarvan.li@derbysoft.net>
2022-12-10 11:22:28 +0800

    chore(script): explain command meaning

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -3,6 +3,7 @@ const execa = require("execa");
 const fs = require("fs");
 (async () => {
   try {
+    // Checkout a new branch named 'gh-pages' started from <start_point> and with no parents
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
@@ -10,12 +11,19 @@ const fs = require("fs");
     // await execa("yarn", ["build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
+    // Stage all the files in the 'folderName'
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
+    // Commit all the staged files in 'folderName' with message 'gh-pages'
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
+    // Push local commit to origin branch 'gh-pages' overriding the restriction that, 
+    // by default, 'git push' refuses to update a remote ref that is not an ancestor of the local ref
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    // Remove the files in the 'folderName' recursively
     await execa("rm", ["-r", folderName]);
+    // Checkout the local branch 'master' ignored any unmerged entries
     await execa("git", ["checkout", "-f", "master"]);
+    // Delete the local branch 'gh-pages'
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");
   } catch (e) {
